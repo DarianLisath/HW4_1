84310c28fe86b6e42708094031a6bf989a6a7349
Darian Lisath <dl023416@ohio.edu>
2018-10-04 18:50:59 -0400

    first commit

--- main.c
+++ main.c
@@ -3,11 +3,22 @@
 
 int main()
 {
+    int i = 1;
+
+    while (i < 100)
+    {
+        printf("%d\n", i);
+        i += 2;
+    }
+
+
+/*
     int i, n;
 
     scanf("%d", &n);
 
     i = 1;
+
     while(i<=n)
     {
         if(i%1==0)
@@ -16,5 +27,6 @@ int main()
         }
         i++;
     }
+    */
     return 0;
 }
